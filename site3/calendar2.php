b7da52a514764f22ac32039edd624d54f703526f
Brian Han <brianyhan@gmail.com>
2013-06-24 22:53:20 -0700

    added some ajax to calendar

---
+++ site3/calendar2.php
@@ -0,0 +1,205 @@
+
+<!DOCTYPE html>
+<html lang="en"><head>
+    <meta charset="utf-8">
+    <title>Brian Y Han | Home</title>
+    <!-- Styles -->
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,300italic,400italic,600,600italic,700,700italic,800italic,800' rel='stylesheet' type='text/css'>
+    <link href="stylesheets/print.css" media="print" rel="stylesheet" type="text/css" /> 
+    <link href="stylesheets/screen.css" media="screen, projection" rel="stylesheet" type="text/css" />
+    <link rel="stylesheet" href="stylesheets/bootstrap.min.css">
+    <link rel="stylesheet" href="stylesheets/bootstrap.css">
+    <script src="js/modernizr-2.6.2-respond-1.1.0.min.js"></script>
+    <script src="js/cvJS.js"></script>
+
+    <link rel="stylesheet" href="stylesheets/font-awesome/css/font-awesome.min.css">
+    <!-- Fav and touch icons -->
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="/apple-touch-icon-144-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="/apple-touch-icon-114-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="/apple-touch-icon-72-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" href="/apple-touch-icon-57-precomposed.png">
+    <link rel="shortcut icon" href="/favicon.png">
+  
+
+  </head>
+
+  <body data-spy="scroll" data-target=".navbar">
+
+    <!--Nav Bar -->
+    <div class="navbar navbar-inverse navbar-fixed-top animated fadeInDownBig">
+        <div class="navbar-inner">
+            <div class="container">
+                <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </a>
+                <a class="brand" href="index.php">Brian Yong Han</a>
+                <div class="nav-collapse collapse">
+                    <ul class="nav pull-right">
+                        <li><a class="scroll" href="#home">Home</a></li>
+                        <li><a class="scroll" href="#work">Projects</a></li>
+                        <li><a class="scroll" href="#about">About Me</a></li>
+                        <li><a class="scroll" href="#interactiveCV">CV</a></li>
+                        <li><a class="scroll" href="#references">References</a></li>
+                      <li> <a class="scroll" href="#news">News</a></li>
+                          <li><a class="scroll" href="calendar.php">Clog</a></li>
+                       
+                        <li><a class="scroll" href="#contact">Contact</a></li>
+                    </ul>
+                </div><!--/.nav-collapse -->
+            </div>
+        </div>
+    </div>
+    
+    
+    <!--News Section-->
+    <section id="news">
+        <div class="container">
+          <div class="row-fluid">
+                 <div class="span12 text-center">
+                   <h1 class="section-title">
+              Clog (Cat Blog)
+            </h1>
+                </div>
+        </div>
+            
+            <div class="row-fluid">
+                
+                <!--Latest News-->
+                <div class="span6">
+                   <h5> News</h5>
+                       
+                       <!--News Article One-->
+                       <div class="row-fluid news-article">
+                           <div class="span2 date pull-left">
+                               <span class="day">12</span><br />
+                               <span class="month">Jul</span>
+                           </div>
+                           
+                           <div class="span9 text-left newsText2">
+                               <h4>Birfday!</h4>
+                               <p>The big 25! Quarter Centurian!</br>
+                                  This year I'll be in Sonoma, AZ </br>
+                                  so I can finally get my first tan!</br>
+                                   LOBSTER status
+                                </p>
+                               
+                               
+                           </div>
+                       </div>
+
+             
+               
+                      
+                       
+                </div><!--End Latest News-->
+                
+                <!--Twitter & Instagram-->
+                <div class="span5 offset1">
+                    <!-- <h5>From Twitter</h5>
+                        <div class="twitter-feed text-left">
+                            <ul id="twitter_update_list" class="rotate"></ul> -->
+                    
+                   
+                    <h5>From Instagram</h5>
+                    <div class="instagram-feed">
+                    
+                    <!--Visit "Snapwidget.com" - Use Settings: 80px wide for images, 3x2 for the grid, and FadeOut on Hover-->
+                    <!-- SnapWidget -->
+                    <a href="http://instagram.com/brianyhan"><img src="img/instagramPic.png"></a>
+                    
+                    </div><!--End Instagram Feed Container-->
+                
+                </div>
+            </div>
+        </div>  
+
+    </section>
+    
+    
+    <!--Contact Section-->
+  
+    <section id="contact">
+        <div class="container">
+            <div class="row-fluid">
+                <div class="span12 text-left">
+                <br>
+      <br>
+          <br>
+      <br>
+                    <h1 class="section-title"><img src="img/blue-line.png" alt="blue-line">Contact Me</h1>
+                </div>
+            </div>  
+            
+            <div class="row-fluid">
+                
+                <!--Contact Info-->
+                <div class="span6 text-left">
+                    <h5>Snail Mail</h5>
+                    <p>1168 East Little Drive <br />
+                    Placentia, CA 92870</p>
+                    <p class="tel"><i class="icon-phone icon-large"></i> &nbsp; 714.900.1650</p><br />
+                    
+                 </div>
+          <div class="span6 text-left">
+            <h5>Find Me On</h5>
+            <ul class="socialize">
+              <li><a class="twitter" href="https://twitter.com/Br1anHan" target="_blank"><i class="icon-twitter icon-2x"></i></a></li>&nbsp;&nbsp;
+              <li><a class="facebook" href="https://www.facebook.com/profile.php?id=1823422" target="_blank"><i class="icon-facebook icon-2x"></i></a></li>&nbsp;&nbsp;
+              <li><a class="google" href="https://plus.google.com/110480429567609983498/posts" target="_blank"><i class="icon-google-plus icon-2x"></i></a></li>&nbsp;&nbsp;
+              <li><a class="instagram" href="http://instagram.com/brianyhan" target="_blank"><i class="icon-camera-retro icon-2x"></i></a></li>&nbsp;&nbsp;
+              <li><a class="linkedin" href="http://www.linkedin.com/pub/brian-han/66/a9b/1b3" target="_blank"><i class="icon-linkedin icon-2x"></i></a></li>&nbsp;&nbsp;
+            </ul>
+            <a class="btn btn-large btn-red scroll" href="mailto:brianyhan@gmail.com"><i class="icon-envelope"></i> Email Me</a>
+          </div>
+            
+        </div>          
+    </section>
+    
+    <footer>
+        <div class="container">
+            <div class="row-fluid">
+                <div class="span4 offset4 text-center">
+                    <p>&copy;Brian Y Han<br />
+                
+                    <a href="http://www.brianyhan.com"><!-- <img src="img/footer-logo.png" alt="Brian Han Logo"> --></a>
+                </div>
+            </div>
+        </div>
+    </footer>
+    
+    <a href="#" class="scrollToTop hidden-phone"><i class="icon-chevron-up"></i></a>
+    
+    
+    <!-- Javascript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="js/jquery-1.9.1.min.js"><\/script>')</script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/bootstrap.js"></script>
+    <script src="js/main.js"></script>
+    <script src="js/jquery.backstretch.js"></script>
+    <script src="js/rotate.js"></script>
+    <script src="js/twitter-feed.js"></script> 
+    <script src="js/cvJS.js"></script>
+    <script src="http://code.jquery.com/ui/1.10.3/jquery-ui.js"></script>
+    <!--Twitter Handle - Insert your Twitter username below ex: (screen_name=USERNAME&include) -->
+    <script src="https://api.twitter.com/1/statuses/user_timeline.json?screen_name=Br1anHan&amp;include_rts=true&amp;count=1&amp;callback=twitterCallback2" type="text/javascript"></script>
+
+    <script>
+    $(document).ready(function() {
+    $.backstretch([
+          // "img/universe1000.jpg",
+          "img/pleiades-wittich.jpg",
+          "img/endeavorCockpit1000.jpg"
+          ], {
+            fade: 1000,
+            duration: 3000
+        });
+    });
+    </script>  
+
+    </body>
+</html>
